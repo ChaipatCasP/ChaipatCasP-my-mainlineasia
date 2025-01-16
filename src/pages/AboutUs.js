c9294fbb5a9214a0bad344baac2f33af75d4ac67
Am OmAm <amomam@Ams-MacBook-Air.local>
2025-01-16 22:45:35 +0700

    update build

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -7,14 +7,14 @@ const AboutUs = () => (
   <section id="about" className="about-us">
     <h2>About Us</h2>
     <div className="about-us-cards">
-      <div className="about-us-card"><img src={section}/></div>
-      <div className="about-us-card"><img src={section}/></div>
-      <div className="about-us-card"><img src={section}/></div>
-      <div className="about-us-card"><img src={section}/></div>
-      <div className="about-us-card"><img src={section}/></div>
-      <div className="about-us-card"><img src={section}/></div>
-      <div className="about-us-card"><img src={section}/></div>
-      <div className="about-us-card"><img src={section}/></div>
+      <div className="about-us-card"><img src={section} alt="about"/></div>
+      <div className="about-us-card"><img src={section} alt="about"/></div>
+      <div className="about-us-card"><img src={section} alt="about"/></div>
+      <div className="about-us-card"><img src={section} alt="about"/></div>
+      <div className="about-us-card"><img src={section} alt="about"/></div>
+      <div className="about-us-card"><img src={section} alt="about"/></div>
+      <div className="about-us-card"><img src={section} alt="about"/></div>
+      <div className="about-us-card"><img src={section} alt="about"/></div>
       </div>
   </section>
 );
