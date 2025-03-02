0c9d7b3712330584dbf82c6cd89f10d8b9025426
Am OmAm <amomam@Ams-MacBook-Air.local>
2025-03-02 18:33:52 +0700

    update 20250302 -2

--- src/pages/AboutUsRailway.js
+++ src/pages/AboutUsRailway.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 import { useLocation } from "react-router-dom"; // ใช้ดึงค่า ID จาก URL
 
 import "./AboutUsRailway.css";
